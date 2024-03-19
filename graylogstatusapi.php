e69bab5a59c2261d277e8d4103bb8e7477674251
wallacemariadeandrade <wallacemariadeandrade@gmail.com>
2024-03-19 15:02:20 -0300

    add api

---
+++ graylogstatusapi.php
@@ -0,0 +1,42 @@
+<?php
+
+function getGraylogServiceStatus() : string {
+    if (strpos(shell_exec("systemctl status graylog-server.service  | grep Active"), "running") !== false) {
+        return "UP";
+    }
+    return "DOWN";
+}
+
+function getMongoDbServiceStatus() :string {
+    if (strpos(shell_exec("systemctl status mongod.service  | grep Active"), "running") !== false) {
+        return "UP";
+    }
+    return "DOWN";
+}
+
+function getElasticsearchServiceStatus() : string {
+    if (strpos(shell_exec("systemctl status elasticsearch.service  | grep Active"), "running") !== false) {
+        return "UP";
+    }
+    return "DOWN";
+}
+
+function getDiskUsagePercent() : float {
+    return doubleval(shell_exec("df -h | grep sda  | awk {'print $5'}"))/100;
+}
+
+function getDiskUsage() : string {
+    return shell_exec("df -h | grep sda  | awk {'print $3'}");
+}
+
+$status = [
+    "graylog" => getGraylogServiceStatus(),
+    "mongodb" => getMongoDbServiceStatus(),
+    "elasticsearch" => getElasticsearchServiceStatus(),
+    "diskusage" => getDiskUsage(),
+    "diskusagepercent" => getDiskUsage()
+];
+
+echo json_encode($status);
+
+?>
